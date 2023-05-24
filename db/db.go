41d2ba274aa34d2319127090f3e2ec89ae1987f1
amandeep <amandeep.chhabra_ftc@external.swiggy.in>
2023-05-24 11:08:57 +0530

    update storer interface methods

--- db/db.go
+++ db/db.go
@@ -9,6 +9,7 @@ type Storer interface {
 	RegisterUser(context.Context, domain.User) error
 	LoginUser(context.Context, string) (domain.LoginDbResponse, error)
 	CreateWallet(context.Context, int64) error
-	GetWallet(context.Context, int) (domain.Wallet, error)
-	CreditWallet(context.Context, int, float64) error
+	GetWallet(context.Context, int64) (domain.Wallet, error)
+	CreditWallet(context.Context, int64, float64) error
+	DebitWallet(context.Context, int64, float64) error
 }
